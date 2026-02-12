0fe5e845df2f1e090ebd7536cd2cd9d8ee0b6006
Kiki <kiki@ebconnect.com>
2026-02-12 15:09:07 +0700

    feat(golang/fx-quickstart): uber-fx dependecy injection quickstarts

---
+++ golang/fx-quickstart/cmd/cli/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+)
+
+func main() {
+	fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			NewHTTPServer,
+			fx.Annotate(
+				NewServeMux, fx.ParamTags(`group:"routes"`)),
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go func() {
+				if err := srv.Serve(ln); err != nil {
+					log.Error("error when starting HTTP server", zap.Error(err))
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{
+		log,
+	}
+}
+
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+func (*EchoHandler) Pattern() string {
+	return "/echo"
+}
+
+func NewServeMux(routes []Route) *http.ServeMux {
+	mux := http.NewServeMux()
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
+	return mux
+}
+
+type Route interface {
+	http.Handler
+
+	Pattern() string
+}
+
+type HelloHandler struct {
+	log *zap.Logger
+}
+
+func NewHelloHandler(log *zap.Logger) *HelloHandler {
+	return &HelloHandler{log}
+}
+
+func (*HelloHandler) Pattern() string {
+	return "/hello"
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Error("Failed to read request", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello, %s\n", body); err != nil {
+		h.log.Error("Failed to write response", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
---
+++ golang/fx-quickstart/go.mod
@@ -0,0 +1,11 @@
+module github.com/khhini/development-sandbox/golang/fx-quickstart
+
+go 1.25.5
+
+require (
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/fx v1.24.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+)
